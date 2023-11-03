6e84e20457c75faa0448ffa2fad79999491316a6
ausin107 <nguyenphuqui396@gmail.com>
2023-11-03 10:34:42 +0700

    update chromedriver

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,2 @@
-import json, os, shutil, re
-import requests, hashlib
-file_list = os.listdir('D:\\douyinVideo\\new')
-  # Khi tạo biến đếm
-count = 1
-  # Lặp qua danh sách các file
-for file_name in file_list:
-    # Tạo tên mới cho file
-    new_file_name = str(count) + '.mp4'
-    # Thay đổi tên file
-    os.rename(os.path.join('D:\\douyinVideo\\new', file_name), os.path.join('D:\\douyinVideo\\new', new_file_name))
-    count += 1
+
+    cookies = pickle.load(f)
